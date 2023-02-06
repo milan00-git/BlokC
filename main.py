64b119090384d517ecba2b084c2eeb272bdb74eb
milan00-git <milan.permana@student.hu.nl>
2023-02-06 11:15:32 +0100

    1e commit blok C test

---
+++ main.py
@@ -0,0 +1,10 @@
+'''Formatieve opdracht 1a: algoritmisch programmeren'''
+
+'''opdracht 1 piramide'''
+
+#manier 1
+lengte = input(int('hoe groot moet piramide zijn?: '))
+a = '*'
+for a in lengte:
+    print(a + lengte)
+    
